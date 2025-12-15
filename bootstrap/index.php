0d16d5c2eb195a741b1ecdeb737c475ca827c0a8
jean-ely.gendrau <150784008+jean-ely-gendrau@users.noreply.github.com>
2025-12-15 11:22:59 +0100

    Réorganisation de la structure HTML pour utiliser une section fluide avec des colonnes Bootstrap

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -9,11 +9,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <main class="container-fluid">
         <h1>Bonjour Bootstrap !</h1>
         <p>Mon premier site avec Bootstrap</p>
         <button class="btn btn-primary">Mon bouton</button>
-    </div>
+
+    <section class="row gap-1 m-2">
+
+        <article class="col-5" style="background-color: lightblue;">
+            Colonne 1 (6 colonnes)
+        </article>
+
+        <article class="col-5" style="background-color: lightcoral;">
+            Colonne 2 (6 colonnes)
+        </article>
+        
+    </section>
+
+    </main>
+
 
 <!-- Link scrip bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>

fc5e34504fdef1ed0411216cbc5ea8ee953c8cde
jean-ely.gendrau <150784008+jean-ely-gendrau@users.noreply.github.com>
2025-12-19 11:30:55 +0100

    Ajout des fichiers de configuration Node.js, y compris .gitignore, package.json et package-lock.json, ainsi que le fichier index.js pour l'affichage des informations sur Node.js.

---
+++ node/hello-node/index.js
@@ -0,0 +1,8 @@
+//Affichage dans le terminal
+console.log("🚀 Hello NodeJs !");
+
+// Infos sur Node.js
+const version = process.versions;
+
+console.log("Version", version);
+console.log("Plateforme:", process.platform);
